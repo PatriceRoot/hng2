6aaefc471fd9f098ade34b65d56665d7a18c7750
PatriceRoot <patricekalwira@gmail.com>
2024-07-08 23:51:38 +0200

    update babel and jest config files

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import dotenv from "dotenv"
 import bodyParser from "body-parser";
 import { PrismaClient } from "@prisma/client";
 import authRoutes from './routes/auth.route.js';
@@ -6,6 +7,7 @@ import userRoutes from './routes/user.route.js';
 import organisationRoutes from './routes/organisation.route.js';
 
 const app = express();
+const port = process.env.PORT || 3000;
 const prisma = new PrismaClient();
 
 app.use(bodyParser.json());
@@ -14,6 +16,6 @@ app.use('/auth', authRoutes);
 app.use('/api', userRoutes);
 app.use('/api', organisationRoutes);
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`); // Start the server and log the port it's running on
 });
